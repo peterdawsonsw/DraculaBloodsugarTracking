2c6adc97453972b0741a751db96191a00f170292
peterdawsonsw <108627075+peterdawsonsw@users.noreply.github.com>
2023-08-18 15:45:15 -0400

    Update bloodsugarDRACULAv1.sh

--- bloodsugarDRACULAv1.sh
+++ bloodsugarDRACULAv1.sh
@@ -7,6 +7,22 @@
 #https://www.baeldung.com/linux/csv-parsing
 #https://linuxize.com/post/bash-functions/
 
+#     This Program to is used to track values via the command line
+#     Copyright (C) 2023  Peter Dawson SW
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see https://www.gnu.org/licenses/.
+
 
 #### Functions ###############################################################################
 
